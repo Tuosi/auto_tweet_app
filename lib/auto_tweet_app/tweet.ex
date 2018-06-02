a47e2a7c98a7efea8cc5b461524ee474baf40418
Wende Lu <wendelu1991@gmail.com>
2018-06-02 19:09:20 +0800

    add AutoTweetApp.Tweet send func

---
+++ lib/auto_tweet_app/tweet.ex
@@ -0,0 +1,13 @@
+defmodule AutoTweetApp.Tweet do
+  def send(str) do
+    config = [
+      consumer_key: System.get_env("AUTO_TWEET_APP_CONSUMER_KEY"),
+      consumer_secret: System.get_env("AUTO_TWEET_APP_COMSUMER_SECRET"),
+      access_token: System.get_env("AUTO_TWEET_APP_ACCESS_TOKEN"),
+      access_token_secret: System.get_env("AUTO_TWEET_APP_ACCESS_SECRET")
+    ]
+
+    ExTwitter.configure(:process, config)
+    ExTwitter.update(str)
+  end
+end
