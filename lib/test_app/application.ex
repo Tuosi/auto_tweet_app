18ec4e0d5aa2ba803720bedb5c9cb631dcbd83f1
Wende Lu <wendelu1991@gmail.com>
2018-06-02 08:06:05 +0800

    init test app

---
+++ lib/test_app/application.ex
@@ -0,0 +1,20 @@
+defmodule TestApp.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: TestApp.Worker.start_link(arg)
+      # {TestApp.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: TestApp.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
