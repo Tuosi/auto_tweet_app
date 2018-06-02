cfd5fc4b95195a810e346a0668e20f900929b780
Wende Lu <wendelu1991@gmail.com>
2018-06-02 08:06:40 +0800

    add get_string_to_tweet

---
+++ lib/test_app/file_reader.ex
@@ -0,0 +1,16 @@
+defmodule TestApp.FileReader do
+  def get_string_to_tweet(path) do
+    File.read!(path)
+    |> String.split("\n")
+    |> Enum.map(&String.trim/1)
+    |> Enum.filter(&(String.length(&1) <= 140))
+  end
+end
+
+# iex -S mix
+
+# cd("lib/test_app")
+# ls
+
+# TestApp.FileReader.get_string_to_tweet("sample.txt")
+
